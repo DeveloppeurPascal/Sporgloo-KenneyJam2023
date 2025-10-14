83f6997c0512b7af7d05ff8cfbd9b106903aac3f
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-14 12:11:11 +0200

    added copyright and timestamp comments in projects source files

--- src-game/uGameData.pas
+++ src-game/uGameData.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Sporgloo
+
+  Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://sporgloo.gamolf.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Sporgloo-KenneyJam2023
+
+  ***************************************************************************
+  File last update : 2025-10-14T10:30:44.118+02:00
+  Signature : f368cc291100abdebc73ef027a22a205a8e306f1
+  ***************************************************************************
+*)
+
 unit uGameData;
 
 interface
